b50c7ea01fa20c06ec3dda3f620f2a81e431f928
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 15:41:50 -0400

    Created while and for loop infinite loops

---
+++ Infinite Loops/Infinite Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinite_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Uncomment to run. But it is an infinite loop so it won't end. 
+            //while (true)
+            //{
+            //    Console.WriteLine("Hello!");
+            //}
+        //Uncomment to run. Another infinite loop. Probably best to not run these.
+            //for (;;)
+            //{
+            //    Console.WriteLine("Hello!");
+            //}
+        }
+    }
+}
