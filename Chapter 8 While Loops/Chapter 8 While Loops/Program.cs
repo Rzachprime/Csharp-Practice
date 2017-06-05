65d828952462f0ca251c7813f360ce3543b50e3a
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 14:07:48 -0400

    Updated project to use github. Created several folders and made first console app for testing while loops

---
+++ Chapter 8 While Loops/Chapter 8 While Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_8_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray01 = new int[] { 18, 42, 26, 44, 55 };
+
+            int loopCounter = 0;
+            int arrayLength = intArray01.Length;
+
+            while (loopCounter < arrayLength)
+            {
+                Console.WriteLine("intArray01[{0}] = {1}", loopCounter, intArray01[loopCounter]);
+
+                loopCounter = loopCounter + 1;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
