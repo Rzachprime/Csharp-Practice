520b0e8f42255285361667f6e581cf55e409f851
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 15:52:13 -0400

    Nested Loop exercise.

---
+++ Nested Loops/Nested Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, k;
+
+            String[] letters = new String[] { "A", "B", "C", "D", "E" };
+
+            for (j = 0; j < letters.Length; j++)
+            {
+                for(k =1; k < 5; k++)
+                {
+                    Console.Write("({0},{1})", letters[j], k);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
