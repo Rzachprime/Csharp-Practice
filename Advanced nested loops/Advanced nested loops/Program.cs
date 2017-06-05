a0107274b4af95040fc618c742cfa2b7b4a23a06
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 17:08:12 -0400

    Advanced nested loops with break statements exercise

---
+++ Advanced nested loops/Advanced nested loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_nested_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] cArray01 = new char[] { 'A', 'B', 'C', 'D', 'E' };
+            for(int j = 0; j < cArray01.Length; j++)
+            {
+                for(int k = 1; k <=10; k++)
+                {
+                    String sf01 = String.Format("{0}{1}", cArray01[j], k);
+                    if (sf01.Equals("C5") == true) break;
+                    Console.Write(sf01); Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
