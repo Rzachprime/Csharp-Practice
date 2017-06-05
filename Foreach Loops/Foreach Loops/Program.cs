4026a4c8f352347ca60aa8fe8ee20152b4090b1f
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 14:49:01 -0400

    Foreach loop version 1. tested out different names for int in the foreach loop to understand it better.

---
+++ Foreach Loops/Foreach Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray01 = new int[] { 18, 42, 26, 44, 55 };
+
+            foreach (int dude in intArray01)
+            {
+                Console.WriteLine("{0}", dude);
+            }
+            Console.ReadKey();
+        }
+    }
+}
