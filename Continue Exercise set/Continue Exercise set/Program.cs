f01ed8c78037a0f2a1e973c06236ff5319940af6
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 16:01:20 -0400

    break and continue exercises

---
+++ Continue Exercise set/Continue Exercise set/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Exercise_set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i == 1001) break;
+                if (i % 5 == 0) continue;
+                Console.WriteLine(i) ;
+            }
+            Console.ReadKey();
+        }
+    }
+}
