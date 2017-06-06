bf15eee401552f06afe0ece4b569317e45980a05
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 20:45:57 -0400

    Wrote basic current datetime program

---
+++ Date and Time Exercises/Date and Time Exercises/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_and_Time_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("The current date & time is now {0}", currentDateTime);
+            Console.ReadKey();
+        }
+    }
+}
