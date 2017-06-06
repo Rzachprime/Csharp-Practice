ee1d3f453d7bd6db800c52edb66d3fcabe22b444
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 23:14:32 -0400

    Basic drive info program

---
+++ PC Enviroment and Information exercises/PC Enviroment and Information exercises/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_Enviroment_and_Information_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Console.WriteLine(myDesktop);
+            Console.ReadKey();
+
+            DriveInfo[] driveList = DriveInfo.GetDrives();
+            int numberOfDrives = driveList.Length;
+            int index = numberOfDrives;
+
+            Console.WriteLine("The number of drives in this computer = {0}", numberOfDrives);
+
+            while (index > 0)
+            {
+                index--;
+                Console.WriteLine("Drive name: {0}", driveList[index].Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
