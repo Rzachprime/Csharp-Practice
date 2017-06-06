8bfe9ada0a16a4715a8c5476ba26579d6cf50db4
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 20:39:41 -0400

    Command line exercise part 3

---
+++ Even more Console work/Even more Console work/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_more_Console_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of args={0}", args.Length);
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("argument[{0}] = 1", i, args);
+
+            }
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please pass exactly two arrguments");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("{0}, {1}", args[1], args[0]);
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
