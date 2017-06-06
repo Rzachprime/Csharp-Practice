b5017b72040e04a8cbe9434b77f0cc5da9f47ab6
Richard Z Troester <rzachprime@gmail.com>
2017-06-06 00:07:20 -0400

    Random number exercises complete

---
+++ Random Number Exercises/Random Number Exercises/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Number_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random01 = new Random();
+
+            for (int count = 0; count < 10; count++)
+            {
+                int value = random01.Next();
+                Console.WriteLine("The next random number is {0}", value);
+            }
+            Console.ReadKey();
+            int seed = 250;
+
+            Random random0 = new Random(seed);
+
+            for(int count = 0; count < 10; count++)
+            {
+                int value = random0.Next();
+                Console.WriteLine("The next random number is {0}", value);
+            }
+            Console.ReadKey();
+
+            Random random03 = new Random();
+            int die01 = random03.Next(1, 6);
+            int die02 = random03.Next(1, 6);
+
+            Console.WriteLine("The value of the 1st die is {0} and the value of the 2cnd is {1}", die01, die02);
+            Console.ReadKey();
+        }
+    }
+}
