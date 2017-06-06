100b5972b0b590fc13226ede6d98a4c895aea7eb
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 22:10:47 -0400

    Collections exercises. Starting by making a list and then adding values to it.

---
+++ Collections exercises/Collections exercises/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList01 = new List<int>();
+            intList01.Add(10);
+            intList01.Add(20);
+            intList01.Add(30);
+            intList01.Add(40);
+            Console.ReadKey();
+        }
+    }
+}
