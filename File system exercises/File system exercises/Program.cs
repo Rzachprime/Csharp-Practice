ab58dcab70e8044df2e5c861b6503f8f470e06f5
Richard Z Troester <rzachprime@gmail.com>
2017-06-05 23:23:49 -0400

    basic computer manipulation stuff

---
+++ File system exercises/File system exercises/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_system_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir01 = Directory.GetCurrentDirectory();
+            string dir02 = Environment.CurrentDirectory;
+            Console.WriteLine(dir01);
+            Console.WriteLine(dir02);
+            Console.ReadKey();
+        }
+    }
+}
